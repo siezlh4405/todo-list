ead22d604c6248773835b645dc063c8acbc6535e
henry.shih <siezlh4405@outlook.com>
2023-03-14 00:20:45 +0800

    feat: todo-list 新增操作

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { listItem } from './todo-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +7,97 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  addText: string = '';
+  doneQty: number = 0;
+  doneRate: number = 0;
+  list: listItem[] = [
+    {
+      method: '洗澡睡覺',
+      isSelected: false,
+      isDone: false,
+    },
+    {
+      method: '看電視',
+      isSelected: false,
+      isDone: false,
+    },
+  ];
+
+  selectQty = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public selectItem(item: listItem) : void {
+    if (item.isSelected) {
+      this.selectQty++;
+    } else {
+      this.selectQty--;
+    }
+  }
+
+  public addItem(): void {
+    const item: listItem = {
+      method: this.addText.trim(),
+      isSelected: false,
+      isDone: false,
+    };
+
+    if (item.method !== '') {
+      this.list.push(item);
+      this.clearAddText();
+    }
+  }
+
+  public setDone(): void {
+    this.list = this.list.map(item => {
+      if (item.isSelected) {
+        item.isDone = true;
+      }
+
+      item.isSelected = false;
+
+      return item;
+    });
+
+    this.calcDoneQty();
+    this.calcDoneRate();
+    this.clearSelected();
+  }
+
+  public deleteItem(): void {
+    this.list = this.list.filter(item => !item.isSelected);
+  }
+
+  public clearDone(): void {
+    this.list = this.list.filter(item => !item.isDone);
+    this.calcDoneQty();
+    this.calcDoneRate();
+  }
+
+  private calcDoneQty(): void {
+    this.doneQty = this.list.filter(item => item.isDone).length;
+  }
+
+  private calcDoneRate(): void {
+    if (this.list.length === 0) {
+      this.doneRate = 0;
+    } else {
+      this.doneRate = (this.doneQty / this.list.length) * 100;
+    }
+  }
+
+  private clearSelected(): void {
+    this.selectQty = 0;
+  }
+
+  private clearAddText(): void {
+    this.addText = '';
+  }
+
+  public isAddTextEmpty(): boolean {
+    return this.addText.trim() === '';
+  }
 }
---
+++ src/app/components/todo-list/todo-list.ts
@@ -0,0 +1,5 @@
+export interface listItem {
+  method: string;
+  isSelected: boolean;
+  isDone: boolean;
+}
